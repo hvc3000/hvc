ece3ca3af72842b7a32a8e58c845f33014604098
hvc3000 <60856900+hvc3000@users.noreply.github.com>
2021-08-02 20:23:08 +0200

    Create rpi_bpow.sh

---
+++ rpi_bpow.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+case "$1" in
+start)
+
+   if [ -e /var/run/bpow ]; then
+   :
+
+   else
+
+   sudo mkdir /var/run/bpow
+   sudo chown pi:pi /var/run/bpow
+
+   fi
+
+   if [ -e /var/run/bpow/nano-work-server.pid ] && [ -e /var/run/bpow/python3.pid ]; then
+   echo boompow is already running
+
+   else
+
+        cd boompow/client
+
+        bin/linux/nano-work-server --cpu-threads  -l 127.0.0.1:7000 &
+        echo $!>/var/run/bpow/nano-work-server.pid
+
+        python3 bpow_client.py --payout ban_1qtjxpycspr5zzq66p4gf3h9t97rq7f9q4gu9buyhr8zgmi73j3aym9t6aun --limit_logging -work any &
+        echo $!>/var/run/bpow/python3.pid
+   fi
+;;
+stop)
+
+
+    kill `cat /var/run/bpow/nano-work-server.pid`
+    rm /var/run/bpow/nano-work-server.pid
+    kill `cat /var/run/bpow/python3.pid`
+    rm /var/run/bpow/python3.pid
+   ;;
+
+restart)
+   $0 stop
+   $0 start
+   ;;
+
+status)
+   if [ -e /var/run/bpow/nano-work-server.pid ]; then
+      echo nano-work-server is running, pid=`cat /var/run/bpow/nano-work-server.pid
+   else
+      echo nano-work-server is NOT running
+   fi
+   if [ -e /var/run/bpow/python3.pid ]; then
+      echo bpow_client is running, pid=`cat /var/run/bpow/python3.pid`
+   else
+      echo bpow_client is NOT running
+      exit 1
+   fi
+   ;;
+*)
+   echo "Usage: $0 {start|stop|status|restart}"
+esac
+exit 0
